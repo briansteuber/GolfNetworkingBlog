d66afdced3e7b921f662e774185792eeb24e4b56
briansteuber <brian.steuber18@gmail.com>
2020-11-24 11:04:54 -0800

    Commit 3 - User can create a new post

--- blog_script.js
+++ blog_script.js
@@ -1,3 +1,31 @@
+var newPostClicked = false;
+var numDivs = 0;
+var theFinalID;
+var theFinalClass;
+
+function submitPost() {
+    console.log("working");
+}
+
+function createPost(element) {
+    newPostClicked = !newPostClicked;
+    if (newPostClicked == true) {
+        $('<div id ="newPost" class="card20">\
+        <br>\
+        <input type="text" name="articleTitle" class="articleTitle" placeholder="Article Title">\
+        <br>\
+        <br>\
+        <textarea name="paragraphText" class="paragraphText" cols="50" rows="10" placeholder="Blog Text"></textarea>\
+        <br>\
+        <br>\
+        <button type="button" id="submitPost" onclick="submitPost()">Submit Post</button>\
+        <div>').insertAfter("#submit-button"); 
+    }   
+    else {
+        $(".card20").remove();
+    }
+}
+
 function submitTitle() {
     var title = $(".blogName").val();
     $(".blogName").val(""); 
@@ -12,7 +40,6 @@ function submitAbout() {
     $(".submitAbout").html(about);
     document.getElementById("card").style.wordWrap = "break-word";
 }
-
 
 function submitFollow() {
     var follow = $(".followInput").val();
