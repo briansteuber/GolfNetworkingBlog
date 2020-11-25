6fc79073a8e78cef7d4fddaceebc4fbcaa74053a
briansteuber <brian.steuber18@gmail.com>
2020-11-24 18:35:38 -0800

    Commit 8 - Reply is added to the page

--- blog_script.js
+++ blog_script.js
@@ -2,11 +2,16 @@ var newPostClicked = false;
 var numDivs = 0;
 var theFinalID;
 var theFinalClass;
+var theArticleName = "Article Name";
+var theParInput = "Text";
 
 function submitPost() {
     newPostClicked = false;
     var articleName = $(".articleTitle").val();
+    theArticleName = articleName;
+    console.log(theArticleName);
     var paragraphInput = $(".paragraphText").val();
+    theParInput = paragraphInput;
     var theDate1 = Date();
     var theDate2 = paragraphInput.split(' ').join('');
     theFinalID = "#" + theDate2;
@@ -17,6 +22,7 @@ function submitPost() {
     var paragraphText = $("<p>");
     var delete1 = $('<button>');
     var replyButton = $('<button>');
+    var editButton = $('<button>');
     
     delete1.attr("type", "button");
     delete1.attr("id", theDate2);
@@ -27,24 +33,32 @@ function submitPost() {
     $(replyButton).attr("id", theDate2);
     replyButton.text("Reply Button");
 
+    editButton.attr("type", "button");
+    $(editButton).attr("id", theDate2);
+    editButton.text("Edit Button");
+
+
     $(articleTitle).html(articleName);
     $(theDate).html(theDate1);
     $(paragraphText).html(paragraphInput);
-    articleTitle.attr("id", theDate2);
+    articleTitle.attr("id", "h2"+theDate2);
     theDate.attr("id", theDate2);
-    paragraphText.attr("id", theDate2);
+    paragraphText.attr("id", "p"+theDate2);
     delete1.attr("class", theDate2);
     var className = paragraphText.attr('class');
     console.log(className);
     theFinalClass = "." + theDate2;
     $("#newPost").after(delete1);
+    $("#newPost").after(editButton);
     $("#newPost").after(replyButton);
     $("#newPost").after(paragraphText);
     $("#newPost").after(theDate);
     $("#newPost").after(articleTitle);
     $(".card20").remove(); 
+    $(".card40").remove(); 
     $(delete1).click(deletePost);
     $(replyButton).click(replyPost);
+    $(editButton).click(editPost);
 }
 
 function createPost(element) {
@@ -52,7 +66,7 @@ function createPost(element) {
     if (newPostClicked == true) {
         $('<div id ="newPost" class="card20">\
         <br>\
-        <input type="text" name="articleTitle" class="articleTitle" placeholder="Article Title">\
+        <input type="text" name="articleTitle" class="articleTitle" placeholder="Article Title"></input>\
         <br>\
         <br>\
         <textarea name="paragraphText" class="paragraphText" cols="50" rows="10" placeholder="Blog Text"></textarea>\
@@ -71,7 +85,7 @@ function replyPost(element) {
     if (newPostClicked == true) {
         $('<div id ="newPost" class="card30">\
         <br>\
-        <input type="text" name="articleTitle" class="articleTitle" placeholder="Article Title">\
+        <input type="text" name="articleTitle" class="articleTitle" placeholder="Article Title"></input>\
         <br>\
         <br>\
         <textarea name="paragraphText" class="paragraphText" cols="50" rows="10" placeholder="Blog Text"></textarea>\
@@ -86,7 +100,39 @@ function replyPost(element) {
 }
 
 function submitPost2() {
-    console.log("working");
+    newPostClicked = false;
+    articleName = $(".articleTitle").val();
+    paragraphInput = $(".paragraphText").val();
+    var theDate1 = Date();
+    var theDate2 = Date.parse();
+    replyFinalID = "#" + theDate2;
+    $(".articleTitle").val("");
+    $(".paragraphText").val("");
+    var articleTitle = $("<h2>");
+    var theDate = $("<h5>");
+    var paragraphText = $("<p>");
+    var delete1 = $('<button>');
+    delete1.attr("type", "button");
+    $(delete1).attr("id", theDate2);
+    delete1.text("Delete Button");
+    
+    $(articleTitle).html(articleName);
+    $(theDate).html(theDate1);
+    $(paragraphText).html(paragraphInput);
+    $(articleTitle).attr("id", "h2"+theDate2);
+    $(theDate).attr("id", theDate2);
+    $(paragraphText).attr("id", "p"+theDate2);
+    $(paragraphText).attr("class", theDate2);
+    var className = $(paragraphText).attr('class');
+    console.log(className);
+    replyFinalClass = "." + theDate2;
+
+    $("#newPost").after(delete1);
+    $("#newPost").after(paragraphText);
+    $("#newPost").after(theDate);
+    $("#newPost").after(articleTitle);
+    $(".card30").remove(); 
+    $(delete1).click(deletePost2);
 }
 
 
@@ -114,17 +160,19 @@ function submitFollow() {
 
 function deletePost() {
     var currID = $(this).attr('id');
-    $("#" + currID).remove();
-    $("#" + currID).remove();
+    $("#h2" + currID).remove();
+    $("#p" + currID).remove();
     $("#" + currID).remove();   
+    $("#" + currID).remove();
     $("#" + currID).remove();
     $("#" + currID).remove();
 }
 
 function deletePost2() {
     var currID = $(this).attr('id');
+    $("#h2" + currID).remove();
+    $("#p" + currID).remove(); 
     $("#" + currID).remove();
-    $("#" + currID).remove();
-    $("#" + currID).remove();   
     $("#" + currID).remove();
 }
+
